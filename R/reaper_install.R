a92390e2b900b3feecca5b1ae8fda433600c2fd8
rpuggaardrode <r.puggaard@phonetik.uni-muenchen.de>
2025-04-15 15:57:27 +0200

    added REAPER installation method

--- R/reaper_install.R
+++ R/reaper_install.R
@@ -1,7 +1,20 @@
+#' Install REAPER
+#'
+#' Install David Talkin's Robust Epoch and Pitch Estimator program as part
+#' of the `sauceshelf` R package.
+#'
+#' @return Used for side effects.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' reaper_install()
+#' }
 reaper_install <- function() {
   curwd <- getwd()
   on.exit(setwd(curwd))
-  dest <- paste0(system.file('extdata', package='sauceshelf'), '/bin')
+  dest <- paste0(system.file('bin', package='sauceshelf'))
+  if (dir.exists(dest)) stop('REAPER already installed')
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   setwd(dest)
   system('git clone https://github.com/google/REAPER.git')
